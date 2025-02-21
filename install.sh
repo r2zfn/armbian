965c4bbc2a72e841bb2121d126b05415ae0054b9
r2zfn <155978026+r2zfn@users.noreply.github.com>
2025-02-21 10:51:26 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -71,17 +71,21 @@ echo "Make DMRGateway"
 git pull > /dev/null 2<&1
 make > /dev/null 2<&1
 echo "Downloading Service Files..."
-cp -F /opt/DMRGateway/DMRGateway ${BIN} > /dev/null 2<&1
-cp -F /opt/DMRGateway/DMRGateway.ini ${SETC}dmrgateway > /dev/null 2<&1
-cp -F /opt/MMDVMCal/MMDVMCal ${BIN} > /dev/null 2<&1
-cp -F /opt/MMDVMHost/MMDVMHost ${BIN} > /dev/null 2<&1
-cp -F /opt/MMDVMHost/MMDVM.ini ${SETC}mmdvmhost > /dev/null 2<&1
-cp -F /opt/MMDVMHost/RemoteCommand ${BIN} > /dev/null 2<&1
+cp /opt/DMRGateway/DMRGateway ${BIN} > /dev/null 2<&1
+#cp /opt/DMRGateway/DMRGateway.ini ${SETC}dmrgateway > /dev/null 2<&1
+cp /opt/MMDVMCal/MMDVMCal ${BIN} > /dev/null 2<&1
+cp /opt/MMDVMHost/MMDVMHost ${BIN} > /dev/null 2<&1
+#cp /opt/MMDVMHost/MMDVM.ini ${SETC}mmdvmhost > /dev/null 2<&1
+cp /opt/MMDVMHost/RemoteCommand ${BIN} > /dev/null 2<&1
 chmod -R 775 /usr/local/bin > /dev/null 2<&1
 
 echo "Downloading Start Service Files..."
 curl --fail -o ${SBIN}dmrgateway.service -s ${MURL}sbin/dmrgateway.service > /dev/null 2<&1
 curl --fail -o ${SBIN}mmdvmhost.service -s ${MURL}sbin/mmdvmhost.service > /dev/null 2<&1
+curl --fail -o ${SBIN}watchdog.service -s ${MURL}sbin/watchdog.service > /dev/null 2<&1
+curl --fail -o ${SBIN}watchdog -s ${MURL}sbin/watchdog > /dev/null 2<&1
+curl --fail -o ${SBIN}findmodem -s ${MURL}sbin/findmodem > /dev/null 2<&1
+curl --fail -o ${SBIN}mmdvmcal -s ${MURL}sbin/mmdvmcal > /dev/null 2<&1
 curl --fail -o ${SBIN}update.sh -s ${MURL}update.sh > /dev/null 2<&1
 chmod -R 775 /usr/local/sbin > /dev/null 2<&1
 
@@ -90,12 +94,16 @@ curl --fail -o ${LIB}dmrgateway.service -s ${MURL}system/dmrgateway.service > /d
 curl --fail -o ${LIB}dmrgateway.timer -s ${MURL}system/dmrgateway.timer > /dev/null 2<&1
 curl --fail -o ${LIB}mmdvmhost.service -s ${MURL}system/mmdvmhost.service > /dev/null 2<&1
 curl --fail -o ${LIB}mmdvmhost.timer -s ${MURL}system/mmdvmhost.timer > /dev/null 2<&1
+curl --fail -o ${LIB}watchdog.service -s ${MURL}system/watchdog.service > /dev/null 2<&1
+curl --fail -o ${LIB}watchdog.timer -s ${MURL}system/watchdog.timer > /dev/null 2<&1
 
 echo "Enable Service and Reload"
 systemctl enable dmrgateway.service > /dev/null 2<&1
 systemctl enable dmrgateway.timer > /dev/null 2<&1
 systemctl enable mmdvmhost.service > /dev/null 2<&1
 systemctl enable mmdvmhost.timer > /dev/null 2<&1
+systemctl enable watchdog.service > /dev/null 2<&1
+systemctl enable watchdog.timer > /dev/null 2<&1
 systemctl daemon-reload > /dev/null 2<&1
 
 
